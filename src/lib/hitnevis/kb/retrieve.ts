f7a14f7c349ab32d671b41e6e51403dc42770539
mehrshadevilb-cell <mehrshad.evil.b@gmail.com>
2026-09-25 14:48:49 +0330

    feat(hitnevis): connect current chart snapshot to retrieval

--- src/lib/hitnevis/kb/retrieve.ts
+++ src/lib/hitnevis/kb/retrieve.ts
@@ -4,6 +4,7 @@
  */
 
 import { getHitKb, HIT_KB_VERSION, HIT_KB_SOURCE, type HitKbRecord, getHitKbStats } from "./dataset";
+import { CURRENT_TOP_100, HITNEVIS_CHART_DATE, HITNEVIS_CHART_SOURCE } from "./current-chart";
 
 export type RetrieveQuery = {
   genre?: string;
